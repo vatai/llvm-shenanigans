ab7a02c7ad2a8f987c0e0b3887305430d646dfb3
Emil VATAI <emil.vatai@gmail.com>
2025-07-27 22:51:31 +0900

    Try to enable flang in cmake
    https://flang.llvm.org/docs/FlangDriver.html

--- src/main.cc
+++ src/main.cc
@@ -1,48 +1,48 @@
-
 #include <stdlib.h>
 #include <map>
 #include <vector>
 #include <iostream>
 #include <type_traits>
-#ifdef HAVE_ADT_OWNINGPTR_H
-#include <llvm/ADT/OwningPtr.h>
-#else
-#include <memory>
-#endif
-#ifdef HAVE_LLVM_OPTION_ARG_H
-#include <llvm/Option/Arg.h>
-#endif
-#include <llvm/Support/raw_ostream.h>
-#include <llvm/Support/ManagedStatic.h>
-#include <llvm/Support/MemoryBuffer.h>
-#include <llvm/TargetParser/Host.h>
-#include <clang/Basic/Version.h>
-#include <clang/Basic/Builtins.h>
-#include <clang/Basic/FileSystemOptions.h>
-#include <clang/Basic/FileManager.h>
-#include <clang/Basic/TargetOptions.h>
-#include <clang/Basic/TargetInfo.h>
-#include <clang/Driver/Compilation.h>
-#include <clang/Driver/Driver.h>
-#include <clang/Driver/Tool.h>
-#include <clang/Frontend/CompilerInstance.h>
-#include <clang/Frontend/CompilerInvocation.h>
-#include <clang/Basic/DiagnosticOptions.h>
-#include <clang/Frontend/TextDiagnosticPrinter.h>
-#include <clang/Lex/HeaderSearchOptions.h>
-#include <clang/Basic/LangStandard.h>
-#include <clang/Lex/PreprocessorOptions.h>
-#include <clang/Frontend/FrontendOptions.h>
-#include <clang/Frontend/Utils.h>
-#include <clang/Lex/HeaderSearch.h>
-#include <clang/Lex/Preprocessor.h>
-#include <clang/Lex/Pragma.h>
-#include <clang/AST/ASTContext.h>
-#include <clang/AST/ASTConsumer.h>
-#include <clang/Sema/Sema.h>
-#include <clang/Sema/SemaDiagnostic.h>
-#include <clang/Parse/Parser.h>
-#include <clang/Parse/ParseAST.h>
+
+// #ifdef HAVE_ADT_OWNINGPTR_H
+// #include <llvm/ADT/OwningPtr.h>
+// #else
+// #include <memory>
+// #endif
+// #ifdef HAVE_LLVM_OPTION_ARG_H
+// #include <llvm/Option/Arg.h>
+// #endif
+// #include <llvm/Support/raw_ostream.h>
+// #include <llvm/Support/ManagedStatic.h>
+// #include <llvm/Support/MemoryBuffer.h>
+// #include <llvm/TargetParser/Host.h>
+// #include <clang/Basic/Version.h>
+// #include <clang/Basic/Builtins.h>
+// #include <clang/Basic/FileSystemOptions.h>
+// #include <clang/Basic/FileManager.h>
+// #include <clang/Basic/TargetOptions.h>
+// #include <clang/Basic/TargetInfo.h>
+// #include <clang/Driver/Compilation.h>
+// #include <clang/Driver/Driver.h>
+// #include <clang/Driver/Tool.h>
+// #include <clang/Frontend/CompilerInstance.h>
+// #include <clang/Frontend/CompilerInvocation.h>
+// #include <clang/Basic/DiagnosticOptions.h>
+// #include <clang/Frontend/TextDiagnosticPrinter.h>
+// #include <clang/Lex/HeaderSearchOptions.h>
+// #include <clang/Basic/LangStandard.h>
+// #include <clang/Lex/PreprocessorOptions.h>
+// #include <clang/Frontend/FrontendOptions.h>
+// #include <clang/Frontend/Utils.h>
+// #include <clang/Lex/HeaderSearch.h>
+// #include <clang/Lex/Preprocessor.h>
+// #include <clang/Lex/Pragma.h>
+// #include <clang/AST/ASTContext.h>
+// #include <clang/AST/ASTConsumer.h>
+// #include <clang/Sema/Sema.h>
+// #include <clang/Sema/SemaDiagnostic.h>
+// #include <clang/Parse/Parser.h>
+// #include <clang/Parse/ParseAST.h>
 
 #include <flang/Frontend/CompilerInstance.h>
 #include <flang/Frontend/CompilerInvocation.h>
@@ -72,5 +72,10 @@
 int main(int argc, char *argv[])
 {
   std::cout << "Hello, world!" <<std::endl;
+  Fortran::frontend::CompilerInstance *Flang  = new Fortran::frontend::CompilerInstance();
+	// create_diagnostics(Clang);
+	// DiagnosticsEngine &Diags = Clang->getDiagnostics();
+	// Diags.setSuppressSystemWarnings(true);
+  std::cout << "Goodbye cruel world!" <<std::endl;
   return 0;
 }
